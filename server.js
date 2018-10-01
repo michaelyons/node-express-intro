fc5b2e7f92f16be7a6482c4873efb3e874032c9f
Michael J. Lyons <mikelyons7@me.com>
2018-09-30 21:53:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const data = require('./public/mockData.json');
 
-app.get('/', (request, response) => {
-  response.send('hello world');
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
+const errorMessage = (request, response, next) => {
+  response.status(404).send('You are wrong, try again');
+};
+
+app.use(urlLogger, timeLogger);
+
+app.use(express.static('public'));
+
+app.get('/error', errorMessage);
+
+app.get('/json', (request, response) => {
+  response.status(200).json(data);
 });
 
 app.listen(3000, () => {
-  console.log('Express intro running on localhost:3000');
+  console.log('Express Intro running on localhost:3000');
 });
